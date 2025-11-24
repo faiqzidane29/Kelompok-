abd50c9c80045e63ab4b03d15961f23c7fa826e4
faiqzidane29 <ahmadfaiqzidane@gmail.com>
2025-11-24 10:09:37 +0000

    Co-authored-by: nadisyaazkia <nadisyaazkia-cpu@users.noreply.github.com> Co-authored-by: dyynn555 <dyynn555@users.noreply.github.com>

---
+++ satu.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+    bool running = true;
+    int x;
+
+    while(running){
+        printf("Masukkan angka (999 untuk stop): ");
+        scanf("%d", &x);
+
+        if(x == 999){
+            running = false; // menghentikan loop
+        } else {
+            printf("Anda memasukkan %d\n", x);
+        }
+    }
+
+    printf("Program selesai");
+}
